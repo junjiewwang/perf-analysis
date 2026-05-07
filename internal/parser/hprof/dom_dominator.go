db293d5052fd5fdefdb30dbf1d36d524c5d58431
junjiewwang <junjiewwang@tencent.com>
2026-05-07 11:17:02 +0800

    refactor(P2): remove internal/parser/hprof legacy package
    Delete the entire internal/parser/hprof package which contained legacy
    type aliases, delegate functions, and the old refgraph.bin serialization
    path. All functionality has been consolidated into perflib/parser/hprof.
    
    Removed files (~4,100 lines):
    - analysis_biggest_objects.go, analysis_retained_calc/debug.go
    - analysis_retainer.go, core_reader.go, core_result_builder.go
    - dom_dominator.go, dom_hierarchical.go, dom_parallel.go
    - graph_buffer_pool.go, graph_gc_root.go, graph_indexed.go
    - graph_reference.go, parallel_analyzer.go, parser.go
    - proto/refgraph.pb.go, proto/refgraph.proto
    - serial_async.go, serial_serializer.go, types.go
    - util_bitset.go, util_compression.go, util_mmap_store.go
    - util_worker_pool.go, worker_pool_test.go, doc.go

--- internal/parser/hprof/dom_dominator.go
+++
@@ -1,7 +0,0 @@
-// Package hprof provides parsing functionality for Java HPROF heap dump files.
-//
-// This file previously contained the dominator tree computation logic.
-// All functionality has been moved to perflib/parser/hprof.
-// Methods on ReferenceGraph (ComputeDominatorTree, SetRetainedSizeStrategy, etc.)
-// are automatically available through the ReferenceGraph type alias.
-package hprof
